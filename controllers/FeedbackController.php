c8b29cd51578a7f9481f05f758e224aa3f60357a
Sergey <filinsrg@gmail.com>
2017-08-30 13:57:50 +0300

    v1.0.2 fixs use Response

--- controllers/FeedbackController.php
+++ controllers/FeedbackController.php
@@ -9,6 +9,7 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\Response;
 
 /**
  * FeedbackController implements the CRUD actions for Feedback model.
